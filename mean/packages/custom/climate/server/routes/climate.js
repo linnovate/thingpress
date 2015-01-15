e93d977e1886617d3ebe8670d170f88798788bb8
AlmogBaku <almog.baku@gmail.com>
2015-01-15 20:17:21 +0200

    routing climate

--- mean/packages/custom/climate/server/routes/climate.js
+++ mean/packages/custom/climate/server/routes/climate.js
@@ -1,10 +1,21 @@
 'use strict';
 
+var climate = require('../controllers/climate');
+
 // The Package is past automatically as first parameter
 module.exports = function(Climate, app, auth, database) {
 
-  app.get('/api/climate', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
+  app.route('/api/climate')
+    .get(climate.all)
+    .post(climate.create)
+  ;
 
+  // Show single
+  app.route('/api/climate/:climate')
+    .get(climate.show)
+    .put(climate.update) //edit only with id
+    .delete(climate.destroy) //delete only with id
+  ;
+
+  app.param('climate', climate.climate);
 };
